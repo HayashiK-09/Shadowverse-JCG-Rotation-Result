529218ce15c8b0b50263c033a27d26209d329f2b
Hayashi_Kohei <hayashi_k0hei@yahoo.co.jp>
2021-01-13 00:22:54 +0900

    簡易カレンダー追加

--- Shadowverse JCG Rotation Result/Form1.Designer.cs
+++ Shadowverse JCG Rotation Result/Form1.Designer.cs
@@ -29,13 +29,30 @@ namespace Shadowverse_JCG_Rotation_Result
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(301, 184);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(866, 527);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
     }
 }
 
